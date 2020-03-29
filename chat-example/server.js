d2ae4fb10e2c58a6aa6fec955349eebef3646201
Matteo Oldani <matteoldani@MBP-di-Matteo-8.fritz.box>
2020-03-29 15:17:11 +0200

    teo

---
+++ chat-example/server.js
@@ -0,0 +1,73 @@
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers
+} = require("./utils/users");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+// set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+const botname = "God";
+
+// run when a client connects
+io.on("connection", socket => {
+  // join room
+  socket.on("joinRoom", ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+
+    socket.join(user.room);
+
+    // welcome current user
+    socket.emit("message", formatMessage(botname, "welcome to chat"));
+
+    // broadcast when user connects
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        formatMessage(botname, `${user.username} has joined the chat`)
+      );
+
+    // send users and room info
+    io.to(user.room).emit("roomUsers", {
+      room: user.room,
+      users: getRoomUsers(user.room)
+    });
+  });
+
+  // listen for chat message
+  socket.on("chatMessage", msg => {
+    user = getCurrentUser(socket.id);
+    io.to(user.room).emit("message", formatMessage(user.username, msg));
+  });
+
+  // broadcast when user disconnets
+  socket.on("disconnect", () => {
+    const user = userLeave(socket.id);
+    if (user) {
+      io.to(user.room).emit(
+        "message",
+        formatMessage(botname, `${user.username} has left the chat`)
+      );
+      // send users and room info
+      io.to(user.room).emit("roomUsers", {
+        room: user.room,
+        users: getRoomUsers(user.room)
+      });
+    }
+  });
+});
+
+const PORT = 3000 || process.env.PORT;
+
+server.listen(PORT, () => console.log("server running on port: " + PORT));
---
+++ commit.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git add app.js
+git commit -m $1
+git push -u origin development
