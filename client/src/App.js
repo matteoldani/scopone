e1a3ae3cfb7d2a193a930ddff67cb744332608c6
Alberto Mosconi <pitagora99@gmail.com>
2020-04-10 12:14:17 +0200

    handle socket connection with component container

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,13 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Join from "./components/Join";
-import Table from "./components/Table";
-import Team from "./components/Team";
-import FineMano from "./components/FineMano";
-import FinePartita from "./components/FinePartita";
+import GameContainer from "./components/GameContainer";
 
 const App = () => {
   return (
     <Router>
       <Route path="/" exact component={Join} />
-      <Route path="/team/:table/:username" exact component={Team} />
-      <Route path="/table" exact component={Table} />
-      <Route path="/finemano" exact component={FineMano} />
-      <Route path="/finepartita" exact component={FinePartita} />
+      <GameContainer />
     </Router>
   );
 };
