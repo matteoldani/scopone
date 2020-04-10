a2d951280be7150ddb27ad2a7c52a2e563a1a0fb
Matteo Oldani <matteoldani@Matteos-iMac.fritz.box>
2020-04-10 15:05:26 +0200

    campo

--- app.js
+++ app.js
@@ -105,6 +105,7 @@ io.on("connection", (socket) => {
 });
 
 var giocaMano = function (players, puntiPrimoTeam, puntiSecondoTeam) {
+  var socketsList;
   var mazzo = makeDeck();
   //mischio il mazzo
   var numeri = estrazioneCasuale();
@@ -140,4 +141,108 @@ var giocaMano = function (players, puntiPrimoTeam, puntiSecondoTeam) {
     players[j / 10].mano = mani[j / 10];
     io.to(players[j / 10].id).emit("playerData", { player: players[j / 10] });
   }
+  //salvo la lista dei socket dei giocatori
+  for(var i =0; i<4; i++){
+    socketsList = io.sockets.connected[players[i].id];
+  }
+
+   //variabili usate da tutti i socket e reimpostate a 0 ogni vola che un nuova carta è giocata
+
+   var presa = 0;
+   var somma = 0;
+   var asso = 0;
+   var carte = [];
+
+  socketsList[0].on("card", ({data}) => {
+    presa = 0;
+    somma = 0;
+    asso = 0;
+    carte.splice(0, carte.length);
+
+    console.log("Carta giocata: ", data);
+
+    //metto in pausa il giocatore
+    players[0].isPlaying = 0;
+    io.to(players[0].table).emit("tablePlayers", {
+      table: players[0].table,
+      players: getTablePlayer(players[0].table),
+    });
+
+    console.log("Rimuovo la caera giocata dalla mano del giocatore");
+    for (var i = 0; i < mani[0].length; i++) {
+      if (mani[0][i].valore == data.valore && mani[0][i].seme == data.seme) {
+        console.log("dentro il primo if");
+        mani[0].splice(i, 1); //elimino la carta giocata
+        console.log(
+          "la carta giocata era la numero: ",
+          data,
+          "la nuova mano è: ",
+          mani[0]
+        );
+      }
+    }
+
+    if (data.valore == 1) {
+      //se trovo l'asso aggiugo il campo alla presa e lo svuto
+      console.log("e' stato giocato un asso");
+      prese1.push(data);
+      for (var i = 0; i < campo.length; i++) {
+        prese1.push(campo[i]);
+        console.log("aggiungo carte anche tra quelle da toglire dal campo");
+        carte.push(campo[i]);
+        ultimaPresa = 1;
+      }
+      //svuoto il campo
+      campo.splice(0, campo.length);
+      asso = 1;
+    }
+
+    if (asso == 0) {
+      console.log("controllo se la carta è in campo e posso prendere");
+      for (var i = 0; i < campo.length; i++) {
+        if (campo[i].valore == data.valore) {
+          console.log("ho trovato una carta uguale, la prendo");
+          prese1.push(campo[i]);
+          prese1.push(data);
+          ultimaPresa = 1;
+          presa = 1;
+
+          carte.push(campo[i]);
+          carte.push(data);
+          
+          //svuoto l'array carte cos' da poterlo riuatilizzare
+          carte.splice(0, carte.length);
+          if (campo.length == 1) {
+            scope1.push(data);
+            console.log("ho anche fatto scopa");
+          }
+          console.log("questo è il campo prima dello splice: \n", campo);
+          campo.splice(i, 1);
+          console.log("ho eliminato la carta del campo: \n", campo);
+        }
+      }
+
+    //mando il campo e l'ultima carta giocata a tutti i players
+    io.to(players[0].table).emit("tableCards", {
+      campo: campo,
+      lastPlayedCard: data,
+    });
+
+  });
+
+  socketsList[1].on("card", ({data}) => {
+
+  });
+
+  socketsList[2].on("card", ({data}) => {
+
+  });
+
+  socketsList[3].on("card", ({data}) => {
+
+  });
+
+
+
+ 
 };
